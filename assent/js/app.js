a42f9247135f45f6f51450a70fff88d0b13b351f
kpndev <kpndev@localhost.localdomain>
2020-11-30 16:19:43 +0530

    notification btn added

--- assent/js/app.js
+++ assent/js/app.js
@@ -138,39 +138,7 @@ jQuery(window).load(function(){
   jQuery(".loader").fadeOut(500);
 });
 
-const bg=document.getElementById('particles-js');
-        window.addEventListener('scroll',function(){
-            bg.style.backgroundSize= 140 - +window.pageYOffset/12+'%'
-           
-        });
 
 
-          // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item'
-    });
 
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-      aos_init();
-    });
-
-    // Initiate venobox (lightbox feature used in portofilo)
-    $(document).ready(function() {
-      $('.venobox').venobox();
-    });
-  });
-
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
+ 
